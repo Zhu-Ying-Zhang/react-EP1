d0797682b99df0736c8d052770b262f89c2d7988
Joe <joe0988692542@gmail.com>
2019-01-29 22:03:06 +0800

    note by EP1

---
+++ EP1/webpack.config.js
@@ -0,0 +1,8 @@
+const isProd = 'production' === process.env.NODE_ENV;
+module.exports = {
+    entry : './src/index.js',
+    output : {
+        filename : 'bundle.js'
+    },
+    mode : isProd? 'production' : 'development',
+};
