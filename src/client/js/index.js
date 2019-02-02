45dafdd91de2cfe8d23f23d7398995cf225403e3
Joe <joe0988692542@gmail.com>
2019-02-02 11:13:10 +0800

    add eslint in webpack

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,7 +1,2 @@
-'use script';
-import '../css/index.css';
-
-const nodeENV = process.env.NODE_ENV;
-console.log(nodeENV);
-console.log(nodeENV);
-console.log("HI");
+"use script";
+import "../css/index.css";
