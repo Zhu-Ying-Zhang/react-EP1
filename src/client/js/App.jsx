2d4b1f58fbe8b950cfdba7afd000f9c071e0587c
Joe <joe0988692542@gmail.com>
2019-02-03 00:50:39 +0800

    intro react & add proptypes

--- src/client/js/App.jsx
+++ src/client/js/App.jsx
@@ -1,8 +1,21 @@
 //App.jsx
 import React from "react";
+import PropTypes from "prop-types";
 
-const App = () => {
-  return <div className="App">This is App!</div>;
+//特殊寫法
+const App = (props) => {
+  const {content} = props;
+  return <div className="App">This is App!{content}</div>;
+};
+//正統寫法
+//class App extends React.Component{
+//  render(){
+//    return <div className="app">This is app!</div>;
+//  }
+//}
+
+App.propTypes = {
+  content: PropTypes.string, 
 };
 
 export default App;
--- src/server/hotReload.js
+++ src/server/hotReload.js
@@ -1,17 +1,20 @@
-import webpack from 'webpack';
-import WebpackDevMiddleware from 'webpack-dev-middleware';
-import WebpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig, {hmrConfig} from '../../webpack.config.babel.js'
+import webpack from "webpack";
+import WebpackDevMiddleware from "webpack-dev-middleware";
+import WebpackHotMiddleware from "webpack-hot-middleware";
+import webpackConfig, { hmrConfig } from "../../webpack.config.babel.js";
 
 //new compile實體
 const compile = webpack(webpackConfig);
 //publicPath要記得加  用處在於如果js有import某些東西圖片或字型 他會把字串取代為publicPath的路徑 發布正式會用到
 export const webpackDevMiddleware = WebpackDevMiddleware(compile, {
-	logLevel:hmrConfig.logLevel, publicPath: webpackConfig.output.publicPath
+  logLevel: hmrConfig.logLevel,
+  publicPath: webpackConfig.output.publicPath
 });
 //
 export const webpackHotMiddleware = WebpackHotMiddleware(compile, {
-	log: hmrConfig.log, path: hmrConfig.path, heartbeat: hmrConfig.heartbeat
+  log: hmrConfig.log,
+  path: hmrConfig.path,
+  heartbeat: hmrConfig.heartbeat
 });
 
-export default {webpackDevMiddleware, webpackHotMiddleware};
+export default { webpackDevMiddleware, webpackHotMiddleware };
