1e8a1d7b5f6b82311419a379dbffdb23abaa749f
Joe <joe0988692542@gmail.com>
2019-02-02 11:53:33 +0800

    fix nodemon to watch server file

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,19 @@
 import Express from "express";
-import EnvConfig from '../../config.json';
+import EnvConfig from "../../config.json";
 //const Express = require('express');
 
-const nodeEnv = process.env.NODE_ENV || EnvConfig.NODE_ENV || 'production';
-const isProd = 'production' === nodeEnv;
+const nodeEnv = process.env.NODE_ENV || EnvConfig.NODE_ENV || "production";
+const isProd = "production" === nodeEnv;
 
 const PORT = process.env.PORT || EnvConfig.PORT || "3000";
 //創造實體
 const server = new Express();
 
 //開發階段才載入
-if(!isProd){
-    const hotReloader = require('./hotReload.js');
-    server.use(hotReloader.webpackDevMiddleware);
-    server.use(hotReloader.webpackHotMiddleware);
+if (!isProd) {
+  const hotReloader = require("./hotReload.js");
+  server.use(hotReloader.webpackDevMiddleware);
+  server.use(hotReloader.webpackHotMiddleware);
 }
 //也可以使用以下寫法與上面的結果一樣
 //if(!isProd){
@@ -22,7 +22,7 @@ if(!isProd){
 //   server.use(webpackHotMiddle);
 //}
 
-const renderHtml = (args) => `
+const renderHtml = args => `
 <!DOCTYPE html>
 <html>
   <head>
@@ -37,17 +37,17 @@ const renderHtml = (args) => `
 </html>
 `;
 
-server.use("/css/", Express.static(`${__dirname}/../../src/client/css/`))
+server.use("/css/", Express.static(`${__dirname}/../../src/client/css/`));
 server.use("/js/", Express.static(`${__dirname}/../../dist/client/js/`));
 //如果什麼都沒加 只有一個slash代表是根目錄
 server.get("/", (request, respons) => {
   respons.send(renderHtml());
 });
 
-server.get('/:path', (request,respons) => {
+server.get("/:path", (request, respons) => {
   //因為request.params是一個物件所以需要JSON.stringify來轉譯但不建議因為如果有藏code就會出問題
   respons.send(renderHtml(JSON.stringify(request.params)));
-})
+});
 
 //`的符號為ES6的書寫方式
 server.listen(PORT, () => {
