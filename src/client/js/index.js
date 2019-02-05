0cddf6e0d9a267ff1cae0fe07949dc994e82c249
Joe <joe0988692542@gmail.com>
2019-02-05 15:52:37 +0800

    Add react-router-dom

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,7 +1,13 @@
 "use script";
 import ReactDOM from "react-dom";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "../css/index.css";
 
-ReactDOM.render(<App content="HI"/>, document.getElementById("app-root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App content="HI" />
+  </BrowserRouter>,
+  document.getElementById("app-root")
+);
